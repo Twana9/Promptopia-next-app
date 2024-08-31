3decbf375e2f7b3817b5c6b3610749378af7df1c
twana koye <twana.00569020@gmail.com>
2024-08-31 20:54:19 +0300

    ::

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,6 +5,15 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 export function Nav() {
   const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    async function setProviders() {
+      const response = await getProviders();
+      setProviders(response);
+    }
+    setProviders();
+  }, []);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -24,12 +33,31 @@ export function Nav() {
             <Link href="/create-prompt" className="black_btn">
               Create Post
             </Link>
-            <button type="button " onClick={signOut} className="outline_btn">
+            <button type="button" onClick={signOut} className="outline_btn">
               Sign Out
             </button>
+            <Image
+              src="/assets/images/logo.svg"
+              width={37}
+              height={37}
+              alt="profile picture"
+              className="rounded-full"
+            />
           </div>
         ) : (
-          <></>
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
         )}
       </div>
     </nav>
