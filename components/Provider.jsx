0a9d8a6fd95767f847436324e3a20e29a34d07d2
twana koye <twana.00569020@gmail.com>
2024-09-01 15:50:09 +0300

    m

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,3 +1,5 @@
-export function Provider() {
-  return <h1>Provider</h1>;
+"use client";
+import { SessionProvider } from "next-auth/react";
+export function Provider({ children, session }) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 }
