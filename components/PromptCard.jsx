27ded19aaf9c674b843b065263ced67f4a26474f
twana koye <twana.00569020@gmail.com>
2024-09-21 21:20:50 +0300

    OOO

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,3 +1,45 @@
-export function PromptCard() {
-  return <h1>PromptCard</h1>;
-}
+"use client";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+
+export const PromptCard = ({
+  post,
+  handleTagClick,
+  handleDelete,
+  handleEdit,
+}) => {
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div
+          className="flex-1 flex justify-start
+        items-center gap-3 cursor-pointer"
+        >
+          <Image
+            src={post.creator.image}
+            alt="user_image"
+            height={40}
+            width={40}
+            className="rounded-full object-contain"
+          />
+        </div>
+        <div className="flex flex-col">
+          <h3
+            className="font-satoshi font-semibold
+          text-gray-500"
+          >
+            {post.creator.username}
+          </h3>
+          <p
+            className="font-inter font-sm 
+          text-gray-500"
+          >
+            {post.creator.email}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
