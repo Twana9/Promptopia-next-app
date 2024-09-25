7bc2fea8953fdb960a9f15ccb97f00b53114779d
twana koye <twana.00569020@gmail.com>
2024-09-25 23:04:22 +0300

    ;;;;;;;

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,4 +1,19 @@
-const Profile = () => {
-  return <h1>profile</h1>;
+"use client";
+import { Profile } from "@components/Profile";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+const MyProfile = () => {
+  const handleDelete = async () => {};
+  const handleEdit = () => {};
+
+  return (
+    <Profile
+      name="my"
+      desc="Welcome to your personalized profile page"
+      data={[]}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 export default Profile;
