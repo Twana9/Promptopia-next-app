7dc503b165e9b3c0319612df348390d5f953a13e
twana koye <twana.00569020@gmail.com>
2024-09-28 14:37:50 +0300

    ;po

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,8 +1,22 @@
 import { PromptCard } from "./PromptCard";
-export function Profile({ name, desc, datal, handleEdit, handleDelete }) {
+export function Profile({ name, desc, data, handleEdit, handleDelete }) {
   return (
     <section className="w-full">
-      <h1>{name} Profile</h1>
+      <h1 className="head_text text-left">
+        <span>{name} </span>
+        Profile
+      </h1>
+      <p className="desc"> {desc}</p>
+      <div className="mt-16 prompt_layout">
+        {data.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+          />
+        ))}
+      </div>
     </section>
   );
 }
