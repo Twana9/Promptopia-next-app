127edb9860d84b59faa1036777e894af70d00795
twana koye <twana.00569020@gmail.com>
2024-09-29 20:25:56 +0300

    fertcj

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,13 @@
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+
+export const GET = async (request, { params }) => {
+  try {
+    await connectToDB();
+    const prompt = await Prompt.findById(params.id).populate("creator");
+    if (!prompt) return new Response("Failed to find prompt", { status: 404 });
+    return new Response(JSON.stringify(prompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch prompts", { status: 500 });
+  }
+};
